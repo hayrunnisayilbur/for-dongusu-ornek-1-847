3927598470bb3c0cd8467a9dbb020e7c59499ce1
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-10-23 12:18:30 +0300

    Add files via upload

---
+++ for döngüsü örnek 1-847/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace for_döngüsü_örnek_1_847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lbAd = new System.Windows.Forms.ListBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(73, 124);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(173, 49);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "yaz";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // lbAd
+            // 
+            this.lbAd.FormattingEnabled = true;
+            this.lbAd.Location = new System.Drawing.Point(267, 66);
+            this.lbAd.Name = "lbAd";
+            this.lbAd.Size = new System.Drawing.Size(143, 251);
+            this.lbAd.TabIndex = 1;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(73, 79);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(173, 20);
+            this.txtAd.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lbAd);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox lbAd;
+        private System.Windows.Forms.TextBox txtAd;
+    }
+}
+
---
+++ for döngüsü örnek 1-847/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_döngüsü_örnek_1_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string ad = txtAd.Text;
+
+            for(int i = 0; i<10; i++)
+            {
+                string metin = i.ToString() + " - " + ad;
+                lbAd.Items.Add(metin);
+            }
+        }
+    }
+}
